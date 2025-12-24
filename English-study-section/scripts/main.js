e22959765b8ac7c90304606529680cdb507c4a95
Fatih Kayacı <fatihkayaci@yahoo.com>
2025-12-24 12:05:18 +0300

    tasarımlar düzeltildi.

--- English-study-section/scripts/main.js
+++ English-study-section/scripts/main.js
@@ -3,16 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-placeholder").innerHTML = data;
+
             // Get the current page name
             var path = window.location.pathname;
             var page = path.split("/").pop();
-            // Find the corresponding link and add the active class
-            var links = document.querySelectorAll('.category-link');
-            links.forEach(function(link) {
-                var linkPage = link.getAttribute('href');
-                if (linkPage === page) {
-                    link.classList.add('active');
+
+            // Set the selected option to current page
+            var select = document.getElementById('category-select');
+            if (select) {
+                // Set current page as selected
+                var options = select.options;
+                for (var i = 0; i < options.length; i++) {
+                    if (options[i].value === page) {
+                        select.selectedIndex = i;
+                        break;
+                    }
                 }
-            });
+
+                // Add change event listener for navigation
+                select.addEventListener('change', function() {
+                    if (this.value) {
+                        window.location.href = this.value;
+                    }
+                });
+            }
         });
 });
